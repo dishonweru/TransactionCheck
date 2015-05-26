0a93748d5338687bfaae9e87c308e0130843d4c5
unknown <fbl@HOICT47-L.familyfinance.co.ke>
2015-05-26 15:33:52 +0300

    transaction

---
+++ transaction_table.sql
@@ -0,0 +1,17 @@
+﻿-- Table: transaction
+
+-- DROP TABLE transaction;
+
+CREATE TABLE transaction
+(
+  tran_id character varying(20),
+  tran_ref character varying(20),
+  tran_value character varying(20),
+  tran_origin character varying(20),
+  tran_status character varying(3)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE transaction
+  OWNER TO postgres;
